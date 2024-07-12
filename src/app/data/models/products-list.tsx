42bf1d93b1a9836f7b8584c7a9afa49da4276310
Kateřina Herda <katerina.herda@gmail.com>
2024-07-12 13:24:13 +0200

    feat(products): add temporary data for product loop

---
+++ src/app/data/models/products-list.tsx
@@ -0,0 +1,34 @@
+const productsList = [
+  {
+    productName: "Product1",
+    image: "/1.avif",
+    price: "19 Kč",
+  },
+  {
+    productName: "Product2",
+    image: "/1.avif",
+    price: "29 Kč",
+  },
+  {
+    productName: "Product3",
+    image: "/1.avif",
+    price: "39 Kč",
+  },
+  {
+    productName: "Product4",
+    image: "/1.avif",
+    price: "49 Kč",
+  },
+  {
+    productName: "Product5",
+    image: "/1.avif",
+    price: "59 Kč",
+  },
+  {
+    productName: "Product6",
+    image: "/1.avif",
+    price: "69 Kč",
+  },
+]
+
+export default productsList
