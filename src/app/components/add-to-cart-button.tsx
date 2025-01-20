9b91cbf7c16b7a2b7649c5bebb8649dd8976321a
Kateřina Herda <katerina.herda@gmail.com>
2025-01-20 19:02:49 +0100

    feat: add button to add items to cart and store products in localStorage

--- src/app/components/add-to-cart-button.tsx
+++ src/app/components/add-to-cart-button.tsx
@@ -1,30 +1,66 @@
+"use client"
 import React, { useState } from "react"
 import type { Product } from "./product-item"
+import QuantityInput from "./quantity-input"
 
 type AddToCartButtonProps = {
-  product: Product
+  clickedProduct: Product
 }
 
-export default function AddToCartButton({ product }: AddToCartButtonProps) {
+export default function AddToCartButton({
+  clickedProduct,
+}: AddToCartButtonProps) {
   const [isClicked, setIsClicked] = useState(false)
-
-  // const getCartFromLS = localStorage.getItem("cart")
-  // const cartContent = getCartFromLS ? JSON.parse(getCartFromLS) : []
-  // const cartItemToString = localStorage.setItem("cart", JSON.stringify(product))
+  const [quantity, setQuantity] = useState(1)
 
   const addToCart = () => {
-    localStorage.setItem("cart", JSON.stringify(product))
+    if (typeof window === "undefined") return
+
+    const cart = localStorage.getItem("cart")
+    const cartContent = cart ? JSON.parse(cart) : []
+
+    if (!Array.isArray(cartContent)) {
+      console.error("Error. Resetting cart.")
+      localStorage.setItem("cart", JSON.stringify([]))
+      return
+    }
+
+    const existingProductIndex = cartContent.findIndex(
+      (item: Product) => item.id === clickedProduct.id,
+    )
+
+    if (existingProductIndex !== -1) {
+      cartContent[existingProductIndex].quantity += quantity
+    } else {
+      cartContent.push({ ...clickedProduct, quantity })
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cartContent))
   }
 
   return (
-    <button
-      onClick={() => {
-        setIsClicked((prev) => !prev)
-        addToCart()
-      }}
-      className="uppercase"
-    >
-      {isClicked === true ? <span>✓</span> : <span>buy</span>}
-    </button>
+    <div className="w-3/4 flex justify-end">
+      {isClicked ? (
+        <div className="flex items-center">
+          <QuantityInput
+            value={quantity}
+            onChange={(value: number) => setQuantity(value)}
+          />
+          <button
+            className="ml-2 uppercase"
+            onClick={() => {
+              addToCart()
+              setIsClicked(false)
+            }}
+          >
+            Add
+          </button>
+        </div>
+      ) : (
+        <button className="uppercase" onClick={() => setIsClicked(true)}>
+          Buy
+        </button>
+      )}
+    </div>
   )
 }
