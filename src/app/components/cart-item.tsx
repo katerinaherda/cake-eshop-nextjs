e28e6bfc4ef4bf8b4927b0a6a6592da0f426e883
Kateřina Herda <katerina.herda@gmail.com>
2025-01-20 19:05:00 +0100

    feat: add map loop for displaying cart items and text truncation for mobile

--- src/app/components/cart-item.tsx
+++ src/app/components/cart-item.tsx
@@ -1,25 +1,57 @@
 "use client"
+import { useState, useEffect } from "react"
 import CloseIcon from "../../../public/icons/close-icon"
 
-export default function CartItem() {
-  const getCartFromLS = localStorage.getItem("cart")
-  const cartContent = getCartFromLS ? JSON.parse(getCartFromLS) : []
+export default function Cart() {
+  const [cartContent, setCartContent] = useState<any[]>([])
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const getCartFromLS = localStorage.getItem("cart")
+    if (getCartFromLS) {
+      setCartContent(JSON.parse(getCartFromLS))
+    }
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 500)
+    }
+
+    handleResize()
+    window.addEventListener("resize", handleResize)
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [])
+
+  if (!cartContent.length) return <p>Košík je prázdný</p>
+
+  const truncate = (input: string) =>
+    isMobile && input?.length > 15 ? `${input.substring(0, 15)}...` : input
 
   return (
-    <div className="w-full ">
-      <div className="px-4 pb-1 pt-3 flex justify-between border-b-2">
-        <div className="w-1/6 my-auto">
-          <button className="w-[3rem] h-[3rem]">
-            <CloseIcon />
-          </button>
+    <div className="w-full">
+      {cartContent.map((item) => (
+        <div
+          key={item.id}
+          className="px-4 pb-1 pt-3 flex justify-between border-b-2"
+        >
+          <div className="w-1/6 my-auto">
+            <button className="w-[2rem] h-[2rem] md:w-[3rem] md:h-[3rem]">
+              <CloseIcon />
+            </button>
+          </div>
+          <div className="w-5/6 flex justify-between text-base lg:text-xl xl:text-2xl">
+            <span className="my-auto whitespace-nowrap w-2/3">
+              {truncate(item.title)}
+            </span>
+            <span className="my-auto whitespace-nowrap">{item.quantity}x</span>
+            <span className="my-auto whitespace-nowrap">
+              {item.price * item.quantity} €
+            </span>
+          </div>
         </div>
-
-        <div className="w-5/6 flex justify-between">
-          <span className="my-auto">{cartContent.title}</span>
-          <span className="my-auto">1x</span>
-          <span className="my-auto">{cartContent.price} €</span>
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
