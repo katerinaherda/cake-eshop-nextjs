1be1496dbf3a6f850385714f7580ba3cc0796384
Kateřina Herda <katerina.herda@gmail.com>
2025-01-23 13:16:38 +0100

    feat(homepage): implemented cart badge on hp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+"use client"
+import { useEffect, useState } from "react"
 import CartComponent from "./components/cart-component"
 import ProductItems from "./components/product-items"
+import { useCartSync } from "./hooks/use-cart-sync"
 
-export async function getData() {
+type Product = {
+  id: number
+  title: string
+  price: number
+  quantity: number
+  image: string
+}
+
+async function fetchData() {
   const res = await fetch(
     "https://raw.githubusercontent.com/Katerina8888/cake-fake-api/main/data.json",
   )
@@ -13,16 +24,29 @@ export async function getData() {
   return res.json()
 }
 
-export default async function Store() {
-  const data = await getData()
+export default function Store() {
+  const [data, setData] = useState<Product[]>([])
+  const cartContent = useCartSync()
+  const [itemsInCart, setitemsInCart] = useState(0)
+
+  useEffect(() => {
+    fetchData()
+      .then((fetchedData) => setData(fetchedData))
+      .catch((err) => console.error("Error fetching data:", err))
+  }, [])
+
+  useEffect(() => {
+    setitemsInCart(
+      cartContent.reduce((total, item) => total + item.quantity, 0),
+    )
+  }, [cartContent])
 
   return (
     <>
       <div className="grid grid-cols-2 lg:grid-cols-3 gap-8">
         <ProductItems data={data} />
       </div>
-
-      <CartComponent />
+      <CartComponent numberOfItems={itemsInCart} />
     </>
   )
 }
